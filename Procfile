badf2ce1c33b3123d69f698ddc04bb7f54772b00
tuaaasa <tuaaasa@gmail.com>
2019-01-07 01:31:41 +0900

    Ready to Deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn TweetRefine.wsgi --log-file -
--- TweetRefine/settings.py
+++ TweetRefine/settings.py
@@ -23,7 +23,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+$5n6nztj6sz15&lay+8v#o+(m145_@nwb_i)i6(1d@ixhagha'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
+if not DEBUG:
+    import django_heroku
+    django_heroku.settings(locals())
 
 ALLOWED_HOSTS = []
 
